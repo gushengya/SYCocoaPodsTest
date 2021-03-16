d5e76f6e0397275e99dd7864b80d3a359bc90840
Bleiler <bleiler@163.com>
2021-03-16 09:37:46 +0800

    测试0.0.1版本

---
+++ SYCocoaPodsTest/SYCocoaPodsClassA.m
@@ -0,0 +1,16 @@
+//
+//  SYCocoaPodsClassA.m
+//  SYCocoaPodsTestDemo
+//
+//  Created by 谷胜亚 on 2021/3/11.
+//  Copyright © 2021 谷胜亚. All rights reserved.
+//
+
+#import "SYCocoaPodsClassA.h"
+
+@implementation SYCocoaPodsClassA
++ (void)logA
+{
+    NSLog(@"AAA");
+}
+@end
